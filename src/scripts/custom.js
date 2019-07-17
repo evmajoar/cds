eaef209ecf11adae137a49c5418bae799b6abfa9
Кристина Глазебная <tina250594@gmail.com>
2019-07-17 18:27:03 +0300

    Выполнена верстка header

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -3,6 +3,17 @@
     Version: 1.0.0
     Year: 2019
 */
-$(function() {
+$(function () {
+
+
+    $('.burger-button').on('click', function (e) {
+        e.preventDefault();
+        // $(this).addClass("menu--open");
+        $('.menu').addClass("menu--open");
+    });
+
+    $('.main-header__menu-close').on('click', function (e) {
+        $('.menu').removeClass("menu--open");
+    });
 
 });
