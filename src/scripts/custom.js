ffaa576bad64921dcbec18a3f7822b703991f974
evmajoar <evmajoar@gmail.com>
2019-07-19 23:12:42 +0300

    Разработан прелоадер

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -1,8 +1,13 @@
 /*
-    Senurable Web Factory JS
-    Version: 1.0.0
-    Year: 2019
+  EVMAJOAR JS
+  Version: 1.0.0
+  Year: 2019
 */
 $(function() {
+  
+  // PRELOADER
+  setTimeout(function() {
+    $( '.preloader' ).addClass( 'preloader--hide' );
+  }, 2000);
 
 });
