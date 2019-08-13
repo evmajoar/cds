d395937c152bf8c1c1dc72db8175b31e9c87a714
Artur <evmajoar@gmail.com>
2019-08-13 16:14:43 +0300

    Доработки 2

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -5,6 +5,63 @@
 */
 $(function () {
 
+  /*
+     SLICK SETTINGS
+  */
+  var $layuotsCarousel = $( '.layouts__carousel-list' );
+
+  $layuotsCarousel.each(function() {
+    $(this).on('init afterChange', function(event, slick){
+      var $thatParent = $(this).parent();
+      $thatParent.find('.carousel-switch__number-slide-item:first-of-type').text(slick.currentSlide + 1);
+      $thatParent.find('.carousel-switch__number-slide-item:last-of-type').text(slick.slideCount);
+    }).slick({
+      dots: false,
+      arrows: true,
+      draggable: false,
+      prevArrow: $(this).siblings('.layouts__carousel-row-button').find('.layouts__carousel-prev'),
+      nextArrow: $(this).siblings('.layouts__carousel-row-button').find('.layouts__carousel-next'),
+      infinite: false,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+        }
+      }]
+    });
+  });
+
+  $('.about-us__carousel-list').slick({
+    dots: false,
+    arrows: false,
+    variableWidth: true
+  });
+
+  var $discountsCarousel = $( '.discounts__carousel-list' ),
+    $discountsPrev = $( '.discounts__carousel-prev' ),
+    $discountsNext = $( '.discounts__carousel-next' );
+
+  $discountsPrev.click(function () {
+    console.log("Есть");
+  });
+
+  $discountsCarousel.slick({
+    dots: false,
+    slidesToShow: 3,
+    variableWidth: true,
+    prevArrow: $discountsPrev,
+    nextArrow: $discountsNext,
+    responsive: [{
+      breakpoint: 767,
+      settings: {
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true
+      }
+    }]
+  });
+
+
   // VARS
   var $body = $('body'),
       $preloader = $('.preloader'),
@@ -12,16 +69,14 @@ $(function () {
       $modals = $('.modals'),
       $callModal = $('.js-modal'),
       $modalsOverlay = $('.modals__overlay'),
-      $modalCallback = $('.modals__callback'),
       $modalsClose = $('.modals__close'),
 
-      $adverBanner = $('.main-header__advertising-banner'),
-      $adverText = $('.main-header__advertising-text'),
       $adverClose = $('.main-header__advertising-close'),
 
       $menu = $( '.menu' ),
       $menuBtn = $( '.burger-button' ),
-      $menuClose = $('.main-header__menu-close');
+      $menuClose = $('.main-header__menu-close'),
+      $phones = $( 'input[type="tel"]' );
 
 
   // FUNCTIONS
@@ -50,15 +105,63 @@ $(function () {
       .focus();
   };
 
+  var filter = function (inpRoom, inpTurn) {
+    $filterRow.each(function () {
+      var $that = $(this),
+        $dataRoom = $that.data('room').toString(),
+        $dataTurn = $that.data('turn').toString();
 
-  // PRELOADER
+      if ((inpRoom === $dataRoom || inpTurn === $dataTurn) && (inpRoom === $dataRoom && inpTurn === $dataTurn)) {
+        $(this).css('display', 'table-row');
+        $('.filter-result__conclusion-count').text($(this).css('display', 'table-row').length);
+      } else {
+        $(this).css('display', 'none');
+      }
+    });
+  };
+
+
+  /*
+    MASK
+   */
+  if ( $phones.length ) {
+    $phones.mask("+7 (999) 999-99-99");
+  }
+
+
+  /*
+    ANCHOR
+   */
+  var $anchor = $('.js-anchor');
+
+  if ( $anchor.length ) {
+    $anchor.on('click', function(event) {
+
+      event.preventDefault();
+      var $that = $( this ), hb = $( 'body, html' );
+
+      if ( $that.is( 'button' ) ) {
+        hb.stop().animate({ scrollTop: $( '.' + $that.data( 'id' ) ).offset().top }, 1000, 'swing');
+      } else if ( $that.is( 'a' ) ) {
+        hb.stop().animate({ scrollTop: $( '.' + $that.attr( 'href' ).replace('#', '') ).offset().top }, 1000, 'swing');
+      }
+
+    });
+  }
+
+
+  /*
+    PRELOADER
+   */
   setTimeout(function (a) {
     toggleBodyClassHidden('remove');
     $preloader.addClass('preloader--hide');
   }, 3000);
 
 
-  // MENU
+  /*
+    MENU
+   */
   $menuBtn.click(function () {
     toggleBodyClassHidden();
     $menu.addClass('menu--opened');
@@ -70,46 +173,24 @@ $(function () {
   });
 
 
-  // BANNER
-  $adverBanner.click(function (event) {
-    var $targetTagName = event.target.tagName.toLowerCase();
-
-    if ( $targetTagName === 'button' ) {
-      $adverBanner.removeClass('main-header__advertising-banner--opened');
-    } else {
-      toggleBodyClassHidden();
-      $modals.addClass('modals--opened')
-        .find('.modals__callback')
-        .addClass('popup--opened')
-        .find('input:first')
-        .focus();
-    }
+  /*
+    BANNER
+   */
+  $adverClose.click(function (event) {
+    $('.main-header__advertising-banner').removeClass('main-header__advertising-banner--opened');
   });
 
 
-  // MODAL
+  /*
+    MODALS
+   */
   $callModal.click(function () {
-    var $that = $(this),
-        jModal = 'js-modal--',
-        wModal = 'modals__';
-
-    if ( $that.hasClass(jModal + 'callback') ) {
-      openPopup(wModal + 'callback');
-    } else if ( $that.hasClass(jModal + 'showroom') ) {
-      openPopup(wModal + 'showroom');
-    } else if ( $that.hasClass(jModal + 'premium') ) {
-      openPopup(wModal + 'premium-finish');
-    }
+    var thatData = $(this).data('id').toString();
+    toggleBodyClassHidden();
+    openPopup('modals__' + thatData);
   });
-
-  $modalsClose.click(function () {
-    closePopup();
-  });
-
-  $modalsOverlay.click( function() {
-    closePopup();
-  } );
-
+  $modalsClose.click(closePopup);
+  $modalsOverlay.click(closePopup);
   $(document).keyup(function(e) {
     if (e.keyCode === 27) {
       closePopup();
@@ -117,29 +198,13 @@ $(function () {
   });
 
 
-  //TABS
+  /*
+    TABS
+  */
   var $tabsSwitch = $('.tabs-switch'),
       $tabsSwitchItemClass = 'tabs-switch__item',
       $tabsContent = $('.tabs-content'),
       $tabsContentItemClass = 'tabs-content__item';
-
-  // $('.tabs-content').slick({
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   arrows: false,
-  //   fade: true,
-  //   asNavFor: '.tabs-switch',
-  //   infinite: false
-  // });
-  //
-  // $('.tabs-switch').slick({
-  //   slidesToShow: 3,
-  //   slidesToScroll: 1,
-  //   asNavFor: '.tabs-content',
-  //   dots: true,
-  //   arrows: false,
-  //   focusOnSelect: true
-  // });
 
   $tabsContent.each(function () {
     $(this).children().each(function (index) {
@@ -148,7 +213,7 @@ $(function () {
         .attr('id', 'tab-index-' + (index + 1));
 
       if (index === 0) {
-        $(this).addClass($tabsContentItemClass + '--current')
+        $(this).addClass($tabsContentItemClass + '--current');
       }
 
     });
@@ -173,12 +238,35 @@ $(function () {
     $that.addClass($tabsSwitchItemClass + '--active')
       .siblings()
       .removeClass($tabsSwitchItemClass + '--active')
-      .closest($tabsSwitch)
-      .siblings($tabsContent)
+      .closest('section')
+      .find($tabsContent)
       .children('#' + $thatId)
       .addClass($tabsContentItemClass + '--current')
       .siblings()
       .removeClass($tabsContentItemClass + '--current');
   });
+
+
+  /*
+    FILTER
+   */
+  var $filterForm = $('.selection-premises__form'),
+      $filterRow = $('.filter-result__row'),
+      $filterCheckedRoomVal = $filterForm.find('input[name="room"]:checked').val(),
+      $filterCheckedTurnVal = $filterForm.find('input[name="square"]:checked').val(),
+      $buttonFilterM = $filterForm.find('.selection-premises__form-button');
+
+
+  $filterForm.on('change', function () {
+
+      var $that = $(this),
+        $inpRoomChecked = $that.find('input[name="room"]:checked').val(),
+        $inpTurnChecked = $that.find('input[name="square"]:checked').val();
+
+      filter($inpRoomChecked, $inpTurnChecked);
+
+  });
+
+  $buttonFilterM.on('click', closePopup);
 
 });
--- src/scripts/maksInputs.js
+++
@@ -1,5 +0,0 @@
-var $phones = $( 'input[type="tel"]' );
-
-if ( $phones.length ) {
-  $phones.mask("+7 (999) 999-99-99");
-}
